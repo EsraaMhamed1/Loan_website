172a8b95cd6bfddc91578d443bc0e3b06f878ad4
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2024-08-06 17:16:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import LoanPage from './components/LoanPage'
+// import Modal from './components/Modal';
+const App =()=>{
+     return(
+      <div className='app'>
+       <LoanPage/>
+       {/* <Modal/> */}
+      </div>
+     )
+}
+export default App;
